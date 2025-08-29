afa3ecd128cf17449665a28ed32670399ae7abaf
LPAmorim <lucas.santos058@sptech.school>
2025-08-29 16:33:48 -0300

    Finalizado

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -3,6 +3,26 @@ package school.sptech;
 public class Main {
 
     public static void main(String[] args) {
+        Time t1 = new Time();
+        t1.nome = "Corinthians";
+        t1.registrarVitoria();
+        t1.registrarVitoria();
+        t1.registrarEmpate();
+        t1.registrarDerrota();
 
+        Time t2 = new Time();
+        t2.nome = "Palmeiras";
+        t2.registrarVitoria();
+        t2.registrarVitoria();
+        t2.registrarVitoria();
+
+        System.out.println("=== Informações do Time 1 ===");
+        t1.exibirInformacoes();
+
+        System.out.println("=== Informações do Time 2 ===");
+        t2.exibirInformacoes();
+
+        System.out.println("=== Comparação de Aproveitamento ===");
+        t1.compararAproveitamento(t2);
     }
 }
--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,65 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    Integer vitorias = 0;
+    Integer empates = 0;
+    Integer derrotas = 0;
 
+    public void registrarVitoria(){ vitorias += 1;}
+
+    public void registrarEmpate(){ this.empates += 1; }
+
+    void registrarDerrota(){
+        this.derrotas += 1;
+    }
+
+    Integer getPontos(){
+        return (vitorias * 3) + empates;
+    }
+
+    Integer getTotalPartidas(){
+        return vitorias + derrotas + empates;
+    }
+
+    Integer getAproveitamento(){
+        return ((vitorias * 100) / getTotalPartidas());
+    }
+
+    void compararAproveitamento(Time adversario){
+        if (getAproveitamento() > adversario.getAproveitamento()){
+            String msg = String.format("O time %s tem um aproveitamento maior que o %s.", nome, adversario.nome);
+            System.out.println(msg);
+
+        } else if (getAproveitamento() < adversario.getAproveitamento()) {
+            String msg = String.format("O time %s tem um aproveitamento maior que o time %s.", adversario.nome, nome);
+            System.out.println(msg);
+
+        }else {
+            String msg = String.format("Os times %s e %s têm o mesmo aproveitamento.", nome, adversario.nome);
+            System.out.println(msg);
+        }
+    }
+
+    void exibirInformacoes(){
+        String msg = String.format("""
+                Time: %s
+                Vitórias: %d
+                Empates: %d
+                Derrotas: %d
+                
+                
+                Pontos: %d
+                Total de Partidas: %d
+                Aproveitamento: %d%%
+                
+                ---
+                """,
+                nome,
+                vitorias,
+                empates,
+                derrotas,
+                getPontos(), getTotalPartidas(), getAproveitamento());
+        System.out.println(msg);
+    }
 }
